003e5c130cd030adcc3a648930596579f6851f0a
gadoneitor15 <gadoneitor@gmail.com>
2018-12-04 08:37:37 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>NGG - WebSite</title>
+	<meta name="description" content="NGG es un servidor revolucionario de Minecraft. Puedes unirte a la IP 'play-ngg.com'.">
+	<meta name="keywords" content="NGG, play-ngg.com, Minecraft Server, Minecraft, Fun, Play, Portal, Website, Official">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="utf-8">
+</head>
+<body>
+  <h1>hola mundo </h1>
+</body>
+</html>
